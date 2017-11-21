39e41136796ef7767b71b6ff1c4e941e9633eb73
Choichiro Maji <e175715@ie.u-ryukyu.ac.jp>
2017-11-21 10:29:51 +0900

    レポート4の課題

--- src/report4.java
+++ src/report4.java
@@ -1,6 +1,11 @@
 public class report4 {
     public static void main(String[] args){
-        String str = null;
-        System.out.println(str.length());
+        try{
+            String str = null;
+            System.out.println(str.length());
+        }catch(NullPointerException e){
+            System.out.println("変数strの中身はnullです。");
+            System.out.println(e.getMessage());
+        }
     }
 }
--- src/report4_2.java
+++ src/report4_2.java
@@ -1,6 +1,6 @@
 public class report4_2 {
     public static void main(String[] args) {
-        String str = "3．14";
-        double value = Double.parseDouble(str);
+            String str = "3．14";
+            double value = Double.parseDouble(str);
     }
 }
